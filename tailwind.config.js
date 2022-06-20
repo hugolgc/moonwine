cfedd96f1dad0fd3dedb118ab461b2cc8473ec32
hugolgc <lagache.hugo@hotmail.fr>
2022-06-20 10:36:25 +0200

    Add tailwindcss 🎨

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './assets/**/*.js',
+    './layouts/**/*.liquid',
+    './sections/**/*.liquid',
+    './snippets/**/*.liquid',
+    './templates/**/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//  npx tailwindcss -o ./assets/tailwind.css -w -m
