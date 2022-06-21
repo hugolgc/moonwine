f90b1e24f25127c09aea2361e6519855257e503f
hugolgc <lagache.hugo@hotmail.fr>
2022-06-21 11:02:11 +0200

    🏗 Build Collection Block

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          dark: '#3A3A3A',
+          medium: '#C3C3C3'
+        },
+        red: {
+          DEFAULT: '#CC2F37'
+        },
+        green: {
+          light: '#D9D7B1'
+        }
+      },
       maxWidth: {
         content: '1440px'
       },
       fontFamily: {
         serif: ['Eugusto', 'serif']
+      },
+      boxShadow: {
+        grid: '0 0 0 1px #D9D7B1'
       }
     },
   },
